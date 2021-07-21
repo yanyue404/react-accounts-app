0ab8f4f59d850237be990095f2f29fad09011058
RainBow <xiaoyueyue165@gmail.com>
2021-07-22 00:35:33 +0800

    test: 再来

--- src/api/index.js
+++ src/api/index.js
@@ -15,6 +15,9 @@ export const getAll = () => axios.get(`${api}/api/v1/records`);
 
 
 
+
+
+
 export const create = (body) => axios.post(`${api}/api/v1/records`, body);
 
 export const update = (id, body) =>
